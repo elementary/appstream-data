fe0e05f41a7bf153e4d00756ea38c78248991524
Corentin Noël <corentin.noel@collabora.com>
2022-07-19 11:19:53 +0200

    Simplify the workflow and make sure we are using git to checkout
    Use the build-in way to add a token. Use pushd/popd to keep the working directory.

--- build.sh
+++ build.sh
@@ -18,8 +18,9 @@ EOF
 
 apt-mirror
 
-mkdir /workdir
-cd /workdir
+APPSTREAM_DIR=/workdir
+mkdir ${APPSTREAM_DIR}
+pushd ${APPSTREAM_DIR}
 
 cat <<EOF > asgen-config.json
 {
@@ -40,16 +41,18 @@ EOF
 
 appstream-generator process ${DIST}
 
+popd
+
 # Clear out the old data
-rm -rf /repo/pantheon-data/main/*
-rm -f /repo/debian/appstream-data-pantheon-icons-hidpi.install
-rm -f /repo/debian/appstream-data-pantheon-icons.install
-touch /repo/debian/appstream-data-pantheon-icons-hidpi.install
-touch /repo/debian/appstream-data-pantheon-icons.install
+rm -rf pantheon-data/main/*
+rm -f debian/appstream-data-pantheon-icons-hidpi.install
+rm -f debian/appstream-data-pantheon-icons.install
+touch debian/appstream-data-pantheon-icons-hidpi.install
+touch debian/appstream-data-pantheon-icons.install
 
 # Copy in the new
-cp export/data/${DIST}/main/Components-amd64.yml.gz /repo/pantheon-data/main/pantheon_${DIST}-main_amd64.yml.gz
-for f in export/data/${DIST}/main/icons-*; do
+cp ${APPSTREAM_DIR}/export/data/${DIST}/main/Components-amd64.yml.gz pantheon-data/main/pantheon_${DIST}-main_amd64.yml.gz
+for f in ${APPSTREAM_DIR}/export/data/${DIST}/main/icons-*; do
 
   # Ignore icon archives with no icons
   FILECOUNT=$(tar -tzvvf ${f} | grep -c ^-) || true
@@ -57,13 +60,17 @@ for f in export/data/${DIST}/main/icons-*; do
 
   # Strip a path like export/data/bionic/main/icons-128x128@2.tar.gz down to 128x128@2
   OUTDIR=`basename ${f} .tar.gz | cut -d- -f2`
-  mkdir -p /repo/pantheon-data/main/icons/${OUTDIR}
-  tar -C /repo/pantheon-data/main/icons/${OUTDIR} -xf ${f}
+  mkdir -p pantheon-data/main/icons/${OUTDIR}
+  tar -C pantheon-data/main/icons/${OUTDIR} -xf ${f}
 
   # Add the extracted directory path to the debian install scripts (either HiDPI or not)
   if [[ $OUTDIR == *"@2" ]]; then
-    echo "pantheon-data/main/icons/${OUTDIR}/* usr/share/app-info/icons/elementary-${CHANNEL}-${DIST}-main/$OUTDIR/" >> /repo/debian/appstream-data-pantheon-icons-hidpi.install
+    echo "pantheon-data/main/icons/${OUTDIR}/* usr/share/app-info/icons/elementary-${CHANNEL}-${DIST}-main/$OUTDIR/" >> debian/appstream-data-pantheon-icons-hidpi.install
   else
-    echo "pantheon-data/main/icons/${OUTDIR}/* usr/share/app-info/icons/elementary-${CHANNEL}-${DIST}-main/$OUTDIR/" >> /repo/debian/appstream-data-pantheon-icons.install
+    echo "pantheon-data/main/icons/${OUTDIR}/* usr/share/app-info/icons/elementary-${CHANNEL}-${DIST}-main/$OUTDIR/" >> debian/appstream-data-pantheon-icons.install
   fi
 done
+
+# Change the ownership to the current user
+chown -R $(id -u):$(id -g) pantheon-data
+chown -R $(id -u):$(id -g) debian
